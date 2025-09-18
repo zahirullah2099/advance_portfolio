9a32b770ba29d8c83b3914727a9e392299b077c9
zahir-ullah <zahirullah2099@gmail.com>
2025-09-18 12:36:46 +0500

    changes in about file

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function toggleNavbar() {
       x.className = "navbar";
     }
   }
+ 
+  document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+    const navCollapse = document.querySelector(".navbar-collapse");
+    const navbarToggler = document.querySelector(".navbar-toggler i");
+
+    // Close navbar on link click
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        if (navCollapse.classList.contains("show")) {
+          document.querySelector(".navbar-toggler").click();
+        }
+      });
+    });
+
+    // Change toggler icon between list and x
+    document.querySelector(".navbar-toggler").addEventListener("click", function () {
+      if (navbarToggler.classList.contains("bi-list")) {
+        navbarToggler.classList.replace("bi-list", "bi-x");
+        navbarToggler.classList.add("text-white");
+      } else {
+        navbarToggler.classList.replace("bi-x", "bi-list");
+      }
+    });
+  }); 
+ 
